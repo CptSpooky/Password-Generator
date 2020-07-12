b3b896c9e1b20d92558f2261ffcfc71fefa1c776
CptSpooky <legacyofthedonut@gmail.com>
2020-07-12 00:22:40 -0400

    variable update

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+const generateBtn = document.querySelector("#generate");
 
 // Password function
 function generatePassword(){
@@ -71,8 +71,8 @@ function hasAny(str, anyOf){
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  const password = generatePassword();
+  const passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
